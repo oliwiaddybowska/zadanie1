f38a5d80a4ea047f3ccad9fbb2fd6f05785d2763
Oliwia Dybowska <oliwiaddybowska@gmail.com>
2024-03-13 11:05:08 +0100

    dodanie koła

--- exercises/excercise1.py
+++ exercises/excercise1.py
@@ -1,3 +1,4 @@
+
 bok_a = 10
 pole_kw = bok_a * bok_a
 print("Pole kwadratu wynosi: " + str(pole_kw))
@@ -11,3 +12,9 @@ bok_a = 10
 wysokosc = 4
 pole_tr = bok_a * wysokosc
 print("Pole trójkąta wynosi: " + str(pole_tr))
+
+import math
+r = 3
+pi = math.pi
+pole_kola = pi*r**2
+print("Pole koła wynosi: " + str(pole_kola))
